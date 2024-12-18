2e665ffb8b47a7212b38f15ab6741cf433a94dc2
Wushengyouhan <840835067@qq.com>
2024-12-18 22:43:27 +0800

    256. Resetting Forms

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -8,6 +8,8 @@ export default function Signup() {
     const acquisitionChannel = fd.getAll("acquisition");
     data.acquisition = acquisitionChannel;
     console.log(data);
+
+    event.target.reset();
   }
   return (
     <form onSubmit={handleSubmit}>
