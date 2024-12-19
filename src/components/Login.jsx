481688aea8c3b96ee09c663d6347356aadc53a8c
Wushengyouhan <840835067@qq.com>
2024-12-19 21:28:26 +0800

    259. Validating Input Upon Form Submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,41 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    const enteredEmail = email.current.value;
-    const enyeredPassword = password.current.value;
 
-    console.log(enteredEmail, enyeredPassword);
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log("Sending HTTP request...");
   }
+
   return (
-    <form onSubmit={handleSubmit}>
+    // 这里设置了noValidate属性，表示不使用HTML5的表单验证
+    <form onSubmit={handleSubmit} noValidate>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
